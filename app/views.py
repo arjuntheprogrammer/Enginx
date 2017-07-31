2ef5dd4279fb7344a06ea04b4905470f94c778a2
Arjun Gupta <saimzn4241@gmail.com>
2017-07-31 15:49:35 +0530

    upd

--- app/views.py
+++ app/views.py
@@ -9,10 +9,15 @@ from django.http import JsonResponse
 
 # requests.post(url, params=params, data=json.dumps(data), headers=headers)
 def index(request):
+	ec= request.GET.get("ec")
+	ph= request.GET.get("ph")
+	moisture= request.GET.get("moisture")
+	print(ec, ph, moisture)
+
 	# headers = {'content-type': 'application/json'}
 	# data = {}
 	# params = {'sessionKey': '9ebbd0b25760557393a43064a92bae539d962103', 'format': 'xml', 'platformId': 1}
-	url = 'https://wovkld4mh8.execute-api.ap-south-1.amazonaws.com/vinayak?ec=13&ph=33&moisture=33'
+	url = 'https://wovkld4mh8.execute-api.ap-south-1.amazonaws.com/vinayak?ec='+ec+'&ph='+ph+'&moisture='+moisture
 	r = requests.get(url)
 	print(r.content)
 
